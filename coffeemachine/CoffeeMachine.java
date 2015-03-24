84228ee778fe768ad616b0ffe42389aae28624c6
Amine Hakkou <amine.hakkou.pro@gmail.com>
2015-03-24 22:57:45 +0000

    ok now it's showing a coffee machine

--- coffeemachine/CoffeeMachine.java
+++ coffeemachine/CoffeeMachine.java
@@ -12,9 +12,10 @@ public class CoffeeMachine extends JComponent{
       m_img = Toolkit.getDefaultToolkit().getImage("img.png");
   }
   public void paint(Graphics g){
+    super.paintComponent(g);
     Graphics2D g2d = (Graphics2D) g;
     g2d.setRenderingHints(m_rHint);
-    g2d.drawImage(m_img,0,0,null);
+    g2d.drawImage(m_img,0,0,this);
   }
   private RenderingHints m_rHint;
   private Image m_img;
